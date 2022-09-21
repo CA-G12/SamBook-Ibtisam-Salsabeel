0c6d4b4afc49d191a21369edf27de6e41e159c32
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-21 16:14:10 +0300

    Adding File Structure For Project Relates #1

---
+++ src/components/Cards.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import VideoCard from './VideoCard';
+
+export default class Cards extends Component {
+  render() {
+    return (
+      <div>
+        <VideoCard />
+      </div>
+    )
+  }
+}
