a165e1a04c4202109921083759be4f6a80ee3a52
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-22 00:02:47 +0300

    Fetching Main data from API

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import BookCard from './BookCard';
+import fetchData from './fetchData';
 
 export default class Cards extends Component {
+  state = {
+    data:[]
+  }
+  
+  componentDidMount(){
+    fetchData()
+    .then((data) => this.setState({data: data.items}))
+    
+  }
+  
   render() {
     return (
       <div>
-        <BookCard />
+        <BookCard data = {this.state.data}/>
       </div>
     )
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: props
+    }
+    // console.log(props,55)
+  }
   render() {
     return (
       <div>SearchBar</div>
