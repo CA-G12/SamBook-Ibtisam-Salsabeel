27fae27e94927ac06c498bf4779c95153385adc1
salsabeelomar <omarsalsabeel65@gmail.com>
2022-09-22 12:46:08 +0300

    fix the enter word

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,7 +1,6 @@
 import BookInfo from "./BookInfo";
 
 const BookCard = ({ data }) => {
-  console.log(data.volumeInfo.imageLinks);
   return data.volumeInfo.imageLinks ? (
     <div className="image-card">
       <img src={data.volumeInfo.imageLinks.smallThumbnail} alt="image" />
