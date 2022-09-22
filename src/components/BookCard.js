ec287e58340c4c56a41979f1098b59d347fc9368
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-22 15:36:18 +0300

    Styling Home Page

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,9 +2,10 @@ import BookInfo from "./BookInfo";
 
 const BookCard = ({ data }) => {
   return data.volumeInfo.imageLinks ? (
-    <div className="image-card">
-      <img src={data.volumeInfo.imageLinks.smallThumbnail} alt="image" />
-    </div>
+      <div className="image-card">
+          <img src={data.volumeInfo.imageLinks.smallThumbnail} alt="image" />
+          <p className="title-card">{data.volumeInfo.title.slice(0,20)}</p>
+      </div>
   ) : null;
 };
 
